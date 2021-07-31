c739fdb055f312baabc6340f92f9ca5bcdee0976
Wanderson <wandersonmg18@gmail.com>
2021-07-31 19:09:09 -0300

    Exception handling - findById

--- src/main/java/com/estudos/course/resources/exceptions/StandardError.java
+++ src/main/java/com/estudos/course/resources/exceptions/StandardError.java
@@ -5,10 +5,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant timestamp;
 	private Integer status;
 	private String error;
